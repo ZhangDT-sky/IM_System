ac85a3427fea5859bf8d7262465a40a3eed8fb1c
ZhangDT-sky <485918776@qq.com>
2026-05-03 23:50:51 +0800

    feat: 构建基础server

---
+++ server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	// 业务
+	fmt.Println("连接成功")
+}
+
+// 启动服务接口
+func (this *Server) Start() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+
+	defer listener.Close()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener.Accept err:", err)
+			continue
+		}
+
+		// do handler
+		go this.Handler(conn)
+	}
+}
