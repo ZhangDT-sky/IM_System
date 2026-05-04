527bbb5fa614360fd656d4449d9afde87e3a60d3
ZhangDT-sky <485918776@qq.com>
2026-05-04 20:03:26 +0800

    feat: 添加私聊模式和公聊模式

--- client.go
+++ client.go
@@ -52,6 +52,81 @@ func (client *Client) menu() bool {
 	}
 }
 
+// 查询在线用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return
+	}
+}
+
+// 私聊模式
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+	fmt.Println(">>>>请输入聊天对象[用户名],exit 退出:")
+	fmt.Scanln(&remoteName)
+	for remoteName != "exit" {
+		fmt.Println(">>>>请输入消息内容,exit 退出:")
+		fmt.Scanln(&chatMsg)
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn.Write err:", err)
+					break
+				}
+			}
+		}
+		chatMsg = ""
+		fmt.Println(">>>>请输入消息内容,exit 退出:")
+		fmt.Scanln(&chatMsg)
+	}
+	return
+	client.SelectUsers()
+	fmt.Println(">>>>请输入聊天对象[用户名],exit 退出:")
+	fmt.Scanln(&remoteName)
+}
+
+// 公聊模式
+func (client *Client) PublicChat() {
+	var chatMsg string
+	fmt.Println(">>>>请输入聊天内容，exit退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("client.conn.Write err:", err)
+				break
+			}
+		}
+		chatMsg = ""
+		fmt.Println(">>>>请输入聊天内容，exit退出。")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+// 修改用户名
+func (client *Client) UpdateName() bool {
+	fmt.Println(">>>>请输入用户名:")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("write error:", err)
+		return false
+	}
+	return true
+}
+
 func (client *Client) Run() {
 	for client.flag != 0 {
 		for client.menu() != true {
@@ -60,12 +135,13 @@ func (client *Client) Run() {
 		// 根据flag处理业务
 		switch client.flag {
 		case 1:
-			//
+			client.PublicChat()
 			break
 		case 2:
-			//
+			client.PrivateChat()
 			break
 		case 3:
+			client.UpdateName()
 			break
 		}
 	}
@@ -87,6 +163,9 @@ func main() {
 		fmt.Println(">>>>>连接服务器失败>>>>>")
 		return
 	}
+	// 单独处理server回执消息
+	go client.DealResponse()
+
 	fmt.Println(">>>>>连接服务器成功>>>>>")
 
 	client.Run()
